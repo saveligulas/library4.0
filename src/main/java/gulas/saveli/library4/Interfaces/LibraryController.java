e65013b8e6892111d198ea4de9d83825c9e93973
saveligulas <saveli.gulas@gmail.com>
2023-02-14 17:44:24 +0100

    updates to LibraryController

--- src/main/java/gulas/saveli/library4/Interfaces/LibraryController.java
+++ src/main/java/gulas/saveli/library4/Interfaces/LibraryController.java
@@ -4,4 +4,7 @@ import java.util.Collection;
 
 public interface LibraryController<Object> {
     public Collection<Object> getAll();
+    public Object getById(Long id);
+    public void registerNewObject(Object object);
+    public void deleteById(Long id);
 }
