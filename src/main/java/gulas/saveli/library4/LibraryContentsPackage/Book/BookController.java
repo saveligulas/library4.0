9cd60ea4576b81345f19ca33e6b694239229e5b2
saveligulas <saveli.gulas@gmail.com>
2023-02-14 17:44:02 +0100

    updates to BookController

---
+++ src/main/java/gulas/saveli/library4/LibraryContentsPackage/Book/BookController.java
@@ -0,0 +1,43 @@
+package gulas.saveli.library4.LibraryContentsPackage.Book;
+
+import gulas.saveli.library4.Interfaces.LibraryController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
+
+@RestController
+@RequestMapping(path = "api/v1/book")
+public class BookController implements LibraryController<Book> {
+    private final BookService bookService;
+    private final BookRepository bookRepository;
+    @Autowired
+    public BookController(BookService bookService, BookRepository bookRepository) {
+        this.bookService = bookService;
+        this.bookRepository = bookRepository;
+    }
+
+    @GetMapping
+    @Override
+    public Collection<Book> getAll() {
+        return null;
+    }
+
+    @GetMapping(path = "{bookId}")
+    @Override
+    public Book getById(@PathVariable("bookId") Long bookId) {
+        return bookService.getById(bookId);
+    }
+
+    @PostMapping
+    @Override
+    public void registerNewObject(@RequestBody Book book) {
+        bookService.s
+    }
+
+    @Override
+    public void deleteById(Long id) {
+
+    }
+}
