a8987558b232f24652f00e44a76d0484f1f3a2ac
saveligulas <saveli.gulas@gmail.com>
2023-02-15 11:27:42 +0100

    added Query to BookRepository

--- src/main/java/gulas/saveli/library4/LibraryContentsPackage/Book/BookRepository.java
+++ src/main/java/gulas/saveli/library4/LibraryContentsPackage/Book/BookRepository.java
@@ -1,8 +1,13 @@
 package gulas.saveli.library4.LibraryContentsPackage.Book;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
+    @Query("SELECT b FROM Book b WHERE b.name = ?1")
+    Optional<Book> findBookByName(String name);
 }
