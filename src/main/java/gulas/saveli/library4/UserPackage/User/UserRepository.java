5661f6ef39f0ff840d251d5818a5c228f40b585c
saveligulas <saveli.gulas@gmail.com>
2023-02-16 10:22:33 +0100

    added UserRepository

--- src/main/java/gulas/saveli/library4/UserPackage/User/UserRepository.java
+++ src/main/java/gulas/saveli/library4/UserPackage/User/UserRepository.java
@@ -1,8 +1,12 @@
 package gulas.saveli.library4.UserPackage.User;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends CrudRepository<User, Long> {
+    @Query("SELECT u FROM User u WHERE u.username = :username")
+    public User getUserByUsername(@Param("username") String username);
 }
