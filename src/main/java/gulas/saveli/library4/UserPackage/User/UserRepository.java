b1da6bc87b1852e3468b005fcb60bfd516a62dc7
saveligulas <saveli.gulas@gmail.com>
2023-02-17 22:52:32 +0100

    changed UserRepository to JpaRepository and added queries

--- src/main/java/gulas/saveli/library4/UserPackage/User/UserRepository.java
+++ src/main/java/gulas/saveli/library4/UserPackage/User/UserRepository.java
@@ -1,15 +1,24 @@
 package gulas.saveli.library4.UserPackage.User;
 
+import org.hibernate.boot.jaxb.internal.stax.LocalSchemaLocator;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface UserRepository extends CrudRepository<User, Long> {
-    @Query("SELECT u FROM User u WHERE u.username = :username")
-    public User getUserByUsername(@Param("username") String username);
+import java.time.LocalDate;
+import java.util.Optional;
 
-    @Query("SELECT u FROM User u WHERE u.email = :email")
-    public User getUserByEmail(@Param("email") String email);
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    @Query(value = "SELECT u FROM User u WHERE u.username = ?1", nativeQuery = true)
+    Optional<User> getUserByUsername(String username);
+
+    @Query("SELECT u FROM User u WHERE u.email = ?1")
+    Optional<User> getUserByEmail(String email);
+
+    @Query(value = "SELECT u FROM User u WHERE u.firstName = :firstName AND u.lastName = :lastName AND u.dob BETWEEN :startDate AND :endDate", nativeQuery = true)
+    Optional<User> getUserByNameAndDob(@Param("firstName") String firstName, @Param("lastName") String lastName, @Param("startDate") LocalDate startDate, @Param("endDate") LocalDate endDate);
+
+    User findByEmail(String email);
 }
