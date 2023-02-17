8a4f5bbeeb3fac4e67f9dceae0411b9a09ca8e44
saveligulas <saveli.gulas@gmail.com>
2023-02-17 10:14:50 +0100

    added UserService

---
+++ src/main/java/gulas/saveli/library4/UserPackage/User/UserService.java
@@ -0,0 +1,25 @@
+package gulas.saveli.library4.UserPackage.User;
+
+import gulas.saveli.library4.UserPackage.Role.Role;
+import gulas.saveli.library4.UserPackage.Role.RoleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public UserService(UserRepository userRepository,
+                       RoleRepository roleRepository,
+                       PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+
+}
