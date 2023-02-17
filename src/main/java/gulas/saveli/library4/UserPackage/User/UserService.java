9f2e74bfde3636222b8b9d4f336663f171bf2703
saveligulas <saveli.gulas@gmail.com>
2023-02-17 22:52:48 +0100

    added UserService.saveUser

--- src/main/java/gulas/saveli/library4/UserPackage/User/UserService.java
+++ src/main/java/gulas/saveli/library4/UserPackage/User/UserService.java
@@ -1,10 +1,11 @@
 package gulas.saveli.library4.UserPackage.User;
 
-import gulas.saveli.library4.UserPackage.Role.Role;
 import gulas.saveli.library4.UserPackage.Role.RoleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -21,5 +22,26 @@ public class UserService {
         this.passwordEncoder = passwordEncoder;
     }
 
+    public String saveUser(UserDto userDto) {
+        Optional<User> userOptional = userRepository.getUserByEmail(userDto.getEmail());
+        if(userOptional.isPresent()) {
+            return "user with email + " + userDto.getEmail() + " already exists";
+        }
+        Optional<User> userOptional1 = userRepository.getUserByNameAndDob(userDto.getFirstName(),userDto.getLastName(),userDto.getDob(), userDto.getDob());
+        if(userOptional1.isPresent()) {
+            return "user with same name and date of birth exists";
+        }
+        User user = new User(userDto.getFirstName(),
+                userDto.getLastName(),
+                userDto.getEmail(),
+                passwordEncoder.encode(userDto.getPassword()),
+                userDto.getDob(),
+                roleRepository.findByName("user").orElseThrow(() -> new IllegalStateException("role user does not exist")));
+        userRepository.save(user);
+        return "user saved successfully";
+    }
 
+    public User findUserByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
 }
