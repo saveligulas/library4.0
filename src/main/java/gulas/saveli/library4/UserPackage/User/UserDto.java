56a86667f016ade1280404cb55941a34704697a8
saveligulas <saveli.gulas@gmail.com>
2023-02-17 22:52:06 +0100

    added dob to UserDto

--- src/main/java/gulas/saveli/library4/UserPackage/User/UserDto.java
+++ src/main/java/gulas/saveli/library4/UserPackage/User/UserDto.java
@@ -7,6 +7,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.time.LocalDate;
+
 @Getter
 @Setter
 @NoArgsConstructor
@@ -15,10 +17,13 @@ public class UserDto {
     private Long id;
     @NotEmpty
     private String firstName;
+    @NotEmpty
     private String lastName;
     @NotEmpty(message = "Please enter an email")
     @Email
     private String email;
     @NotEmpty(message = "Please enter a password")
     private String password;
+    @NotEmpty
+    private LocalDate dob;
 }
